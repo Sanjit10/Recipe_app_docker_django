896abbaed24a2f8009bfb7e2ca8c4c57fbd008e4
Sanjit Khanal <Sanjitnepalkhanal@gmail.com>
2023-10-08 11:00:28 +0545

    Setting up user_model

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,47 @@
-from django.db import models # noqa
+"""
+Database models.
+"""
+from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+     PermissionsMixin,
+)
+# AbstractBaseUser contains the functionality for authentication
+# system but not any fields and
 
-# Create your models here.
+# PermissionsMixin contains the functionality for permissions
+# and also the fields needed for the permissions system
+
+class UserManager(BaseUserManager):
+    """Manager for users."""
+
+    def create_user(self, email, password=None, **extra_fields):
+        """Create, return and save a new user."""
+        if not email:
+            raise ValueError('Users must have an email address.')
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password):
+        """Create, return and save a new superuser."""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        return user
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """User in the system"""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True) # can login
+    is_staff = models.BooleanField(default=False) # staff user
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
